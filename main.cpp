95051aa086f070dff5a3a28fb9b9e5b5df4dc58b
jkue1 <jkue1@mail.csuchico.edu>
2018-02-22 16:37:46 -0800

    1part

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
+// Author: Joshua Kue
 #include<iostream>
 #include<string>
 using namespace std;
 
+
 int main()
 {
+
+
   string list[5]; //array of 5 strings
-  int numItems = 0;
+  int n, numItems = 0;
   char input;
 
+do
+{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+}
+
+while (!( input == 'Q' || input == 'q' ));
+
 
   return 0;
 }
